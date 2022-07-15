6f1f9c4c927da649c7103b5bdb7bd4ffb85df944
林兴 <linxing0812@vip.qq.com>
2022-07-15 11:16:56 +0800

    websocket send ping pong and close

---
+++ websocket/websocket_channel.cpp
@@ -0,0 +1,30 @@
+﻿#include <websocket/websocket_channel.h>
+
+void WebsocketChannel::send_ping(const std::string& text)
+{
+	send_ws_content(WsPing, text);
+}
+
+void WebsocketChannel::send_pong(const std::string& text)
+{
+	send_ws_content(WsPong, text);
+}
+
+void WebsocketChannel::send_ws_content(OpCode op, const std::string& text)
+{
+	int len = text.size();
+	int buff_len = pack_len(len);
+	char* buff = (char*)malloc(buff_len);
+	if (buff == NULL)
+	{
+		return;
+	}
+	pack_and_copy(text.c_str(), len, op, buff, buff_len);
+	send_ws_message(buff, buff_len);
+	free(buff);
+}
+
+void WebsocketChannel::send_close()
+{
+	send_ws_content(WsConnectionClose, "");
+}
---
+++ websocket/websocket_channel.h
@@ -0,0 +1,24 @@
+﻿#ifndef UVCORE_WEBSOCKET_CHANNEL_H
+#define UVCORE_WEBSOCKET_CHANNEL_H
+
+#include <websocket/websocket.h>
+#include <string>
+
+class WebsocketChannel
+{
+public:
+	void send_ping(const std::string&);
+	void send_pong(const std::string&);
+	void send_close();
+
+	//传入的数据是websocket格式
+	virtual void send_ws_message(const char* data, int len) = 0;
+
+	//原始格式
+	virtual int writeInLoop(const char* data, int len, OpCode) = 0;
+	virtual int write(const char* data, int len, OpCode) = 0;
+
+	void send_ws_content(OpCode op, const std::string&);
+};
+
+#endif
