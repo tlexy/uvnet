1e2235670650c48e91e73b32528b77ba4c7701eb
林兴 <linxing0812@vip.qq.com>
2022-10-19 12:05:51 +0800

    udp receiver modify

--- core/udp_server.cpp
+++ core/udp_server.cpp
@@ -1,5 +1,6 @@
 ﻿#include "udp_server.h"
 #include "event_loop.h"
+#include <iostream>
 
 NS_UVCORE_B
 
@@ -27,11 +28,12 @@ Udp* UdpServer::addBind(IpAddress& ip, Udp::UdpReceiveCallback2 cb)
 	{
 		_loop->runInLoop(std::bind(&UdpServer::do_bind, this, udp, ip, cb));
 	}
-	if (ret == 0)
+	/*if (ret == 0)
 	{
 		return udp;
 	}
-	return nullptr;
+	return nullptr;*/
+	return udp;
 }
 
 Udp* UdpServer::addPreBind(IpAddress& ip, Udp::UdpReceiveCallback2 cb)
@@ -57,6 +59,8 @@ int UdpServer::do_bind(Udp* udp, IpAddress& ip, Udp::UdpReceiveCallback2 cb)
 	//绑定失败时，调用回调函数，地址传为空
 	if (ret != 0 && cb)
 	{
+		const char* errstr = uv_strerror(ret);
+		std::cerr << "do bind error, return core:" << ret << ", err msg: " << errstr << ",file: " << __FUNCTION__ << ",LINE: " << __LINE__ << std::endl;
 		cb(udp, nullptr);
 	}
 	return ret;
